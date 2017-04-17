f37b89ad03e8796f0cdbeedfb1d9c49152a3ba68
kiwiProgrammer <cheah_kw@hotmail.com>
2017-04-17 22:55:05 +0800

    Added REST API

---
+++ web/api.php
@@ -0,0 +1,19 @@
+<?php
+ 
+// get the HTTP method, path and body of the request
+$method = $_SERVER['REQUEST_METHOD'];
+
+$name = '';
+
+if ($method == 'GET') {
+	$name = $_GET['name'];
+}
+
+if($name) {
+	$result = dns_get_record($name);
+	if(!$result) {
+		http_response_code(404);
+		die();
+	}
+	echo json_encode($result);
+}
